f29a2b451f97e88c7ec971c9784e561802959a0c
Kory Newton <korybnewton@gmail.com>
2019-06-11 20:50:24 -0700

    api change WIP

--- mainbot.py
+++ mainbot.py
@@ -46,57 +46,78 @@ random_select = {}
 rates_data = {}
 
 
-def fixer():
-    global rates_data
+# def fixer():
+#     global rates_data
+#     global random_select
+#     url = main_api + access_key + base + symbols
+#     json_data = requests.get(url).json()
+#     rates_data = dict.items(json_data['rates'])
+#     rates_data = dict(rates_data)
+#     EUR = rates_data['EUR']
+#     BIT = rates_data['BTC']
+#     bitprice = (EUR / BIT)
+#     rates_data.update((x, y*bitprice / 100000000)
+#                       for x, y in rates_data.items())
+#     rates_data['VEF'] /= 100000
+#     rates_data['VES'] = rates_data.pop('VEF')
+#     random_select = random.sample(symbol_key, 12)
+#     compose()
+
+def getData():
     global random_select
-    url = main_api + access_key + base + symbols
-    json_data = requests.get(url).json()
-    rates_data = dict.items(json_data['rates'])
-    rates_data = dict(rates_data)
-    EUR = rates_data['EUR']
-    BIT = rates_data['BTC']
-    bitprice = (EUR / BIT)
-    rates_data.update((x, y*bitprice / 100000000)
-                      for x, y in rates_data.items())
-    rates_data['VEF'] /= 100000
-    rates_data['VES'] = rates_data.pop('VEF')
-    random_select = random.sample(symbol_key, 12)
-    compose()
+    url = 'https://api.coinbase.com/v2/exchange-rates?currency=BTC'
+    try:
+        json_data = requests.get(url).json()['data']['rates']
+
+        for key in emoji_dict:
+            if key in json_data:
+                price_sats = float(json_data[key])
+                rates_data[key] = str(price_sats / 100000000)
+    except:
+        print('request failed')
+
+    # for key in rates_data:
+    #     rates_data[key] = str(rates_data[key])
+
+    print(rates_data)
+
+
+getData()
 
 # compose 2 column tweet
 
 
-def compose():
-    global to_be_tweeted
-    for i in range(len(random_select)):
-        key = random_select[i]
-        emoji = emoji_dict[key]
-        price = '{0:.5f}'.format(rates_data[key])
-        if i % 2 == 0:
-            even_line = '\n' + emoji + ': ' + str(price) + ' ' + key
-            to_be_tweeted = to_be_tweeted + even_line
-        else:
-            odd_line = '   ' + emoji + ': ' + str(price) + ' ' + key
-            to_be_tweeted = to_be_tweeted + odd_line
-        i += 1
-    to_be_tweeted = to_be_tweeted + '\n' + '                       #Bitcoin'
-    tweet()
+# def compose():
+#     global to_be_tweeted
+#     for i in range(len(random_select)):
+#         key = random_select[i]
+#         emoji = emoji_dict[key]
+#         price = '{0:.5f}'.format(rates_data[key])
+#         if i % 2 == 0:
+#             even_line = '\n' + emoji + ': ' + str(price) + ' ' + key
+#             to_be_tweeted = to_be_tweeted + even_line
+#         else:
+#             odd_line = '   ' + emoji + ': ' + str(price) + ' ' + key
+#             to_be_tweeted = to_be_tweeted + odd_line
+#         i += 1
+#     to_be_tweeted = to_be_tweeted + '\n' + '                       #Bitcoin'
+#     tweet()
 
-# tweet function
-
-
-def tweet():
-    global to_be_tweeted
-    global rates_data
-    global random_select
-    api.update_status(to_be_tweeted)
-    print(random_select)
-    print('tweeted')
-    to_be_tweeted = ''
-    rates_data = {}
-    random_select = []
-    time.sleep(5400)
-    fixer()
+# # tweet function
 
 
-fixer()
+# def tweet():
+#     global to_be_tweeted
+#     global rates_data
+#     global random_select
+#     api.update_status(to_be_tweeted)
+#     print(random_select)
+#     print('tweeted')
+#     to_be_tweeted = ''
+#     rates_data = {}
+#     random_select = []
+#     time.sleep(5400)
+#     fixer()
+
+
+# fixer()

6065f8ccb557fb1c6ecc60a0742310de9899d67b
Kory Newton <korybnewton@gmail.com>
2019-06-26 12:22:49 -0700

    wip tweet format

--- streaming.py
+++ streaming.py
@@ -24,12 +24,14 @@ class MyStreamListener(tp.StreamListener):
     def on_status(self, status):
         # filter
         split = status.text.split()
-        currency = [i for i in split if len(i) == 3]
+        currency = [i.upper() for i in split if len(i) == 3]
         for item in currency:
             if item in price_data:
                 self.compose(status, item)
 
     def compose(self, status, curr):
+        larger_baseline = ['JPY', 'INR']
+
         emoji = emoji_dict[curr]
 
         sats = float(price_data[curr])
@@ -37,13 +39,22 @@ class MyStreamListener(tp.StreamListener):
         thousand = sats * 1000
         hundred_k = sats * 100000
         million = sats * 1000000
+        bitcoin = sats * 100000000
 
         single_unit = 1 / sats
+        hundredths_unit = single_unit / 100
         ten_units = single_unit * 10
         hundred_units = single_unit * 100
+        five_hundred_units = single_unit * 500
         thousand_units = single_unit * 1000
+        ten_thousand_units = single_unit * 10000
+        hundred_thousand_units = single_unit * 100000
+        # five_hundred_thousand_units = single_unit * 500000
 
         #  format numbers
+
+        bitcoin = '{0:,}'.format(round(bitcoin))
+
         if sats > 1:
             sats = '{0:.2f}'.format(sats)
         else:
@@ -55,54 +66,101 @@ class MyStreamListener(tp.StreamListener):
             hundred = '{0:.5f}'.format(hundred)
 
         if thousand > 1:
-            thousand = '{0:.2f}'.format(thousand)
+            if thousand > 1000:
+                thousand = '{0:,}'.format(round(thousand))
+            else:
+                thousand = '{0:,}'.format(round(thousand))
         else:
             thousand = '{0:.5f}'.format(thousand)
 
         if hundred_k > 1:
-            hundred_k = '{0:.2f}'.format(hundred_k)
+            hundred_k = '{0:,.2f}'.format(hundred_k)
         else:
             hundred_k = '{0:.5f}'.format(hundred_k)
 
         if million > 1:
-            million = '{0:.2f}'.format(million)
+            if million > 1000:
+                million = '{:,d}'.format(round(million))
+            else:
+                million = '{0:.2f}'.format(million)
         else:
             million = '{0:.5f}'.format(million)
 
+        # satoshi equivalent to currency
         if single_unit > 1:
             single_unit = '{:,d}'.format(round(single_unit))
         else:
-            single_unit = '{0:.5f}'.format(single_unit)
+            single_unit = '{0:.2f}'.format(single_unit)
 
-        if ten_units > 1:
+        if ten_units > 100:
             ten_units = '{:,}'.format(round(ten_units))
         else:
-            ten_units = '{0:.5f}'.format(ten_units)
+            ten_units = '{0:.2f}'.format(ten_units)
 
-        if hundred_units > 1:
+        if hundred_units > 100:
             hundred_units = '{:,}'.format(round(hundred_units))
         else:
-            hundred_units = '{0:.5f}'.format(hundred_units)
+            hundred_units = '{0:.2f}'.format(hundred_units)
 
-        if thousand_units > 1:
+        if thousand_units > 100:
             thousand_units = '{:,}'.format(round(thousand_units))
         else:
-            thousand_units = '{0:.5f}'.format(thousand_units)
+            thousand_units = '{0:.2f}'.format(thousand_units)
+
+        if five_hundred_units > 100:
+            five_hundred_units = '{:,}'.format(round(five_hundred_units))
+        else:
+            five_hundred_units = '{0:.2f}'.format(five_hundred_units)
+
+        if ten_thousand_units > 100:
+            ten_thousand_units = '{:,}'.format(round(ten_thousand_units))
+        else:
+            ten_thousand_units = '{0:.2f}'.format(ten_thousand_units)
+
+        if hundred_thousand_units > 100:
+            hundred_thousand_units = '{:,}'.format(
+                round(hundred_thousand_units))
+        else:
+            hundred_thousand_units = '{0:.2f}'.format(hundred_thousand_units)
+
+        if hundredths_unit > 1:
+            hundredths_unit = '{:,}'.format(round(hundredths_unit))
+        else:
+            hundredths_unit = '{0:.2f}'.format(hundredths_unit)
 
             # '\n' + f'100,000 #satoshis = {hundred_k} ${curr}' + \
-        text = '      ' + emoji + \
-            '\n' + f'1 #satoshi = {sats} ${curr}' + \
-            '\n' + f'100 #satoshis = {hundred} ${curr}' + \
-            '\n' + f'1,000 #satoshis = {thousand} ${curr}' + \
-            '\n' + f'1,000,000 #satoshis = {million} ${curr}' + \
-            '\n' + '------------------------------------' + \
-            '\n' + f'1 ${curr} = {single_unit} #satoshis' + \
-            '\n' + f'10 ${curr} = {ten_units} #satoshis' + \
-            '\n' + f'100 ${curr} = {hundred_units} #satoshis' + \
-            '\n' + f'1,000 ${curr} = {thousand_units} #satoshis'
-        # print(text)
-        # self.update_status(text, status.id)
-        self.tweet_it(text, status.id)
+
+        if curr in larger_baseline:
+            text = f'{emoji} ${curr} to #satoshi conversions:' + '\n' + \
+                '\n' + f'   1 {curr} = {single_unit} sats' + \
+                '\n' + f'   500 {curr} = {five_hundred_units} sats' + \
+                '\n' + f'   10,000 {curr} = {ten_thousand_units} sats' + \
+                '\n' + f'   100,000 {curr} = {hundred_thousand_units} sats' + \
+                '\n' + '\n' + \
+                '\n' + f'   1 sat = {sats} {curr}' + \
+                '\n' + f'   100 sats = {hundred} {curr}' + \
+                '\n' + f'   1,000 sats = {thousand} {curr}' + \
+                '\n' + f'   1,000,000 sats = {million} {curr}' + \
+                '\n' + '\n' + \
+                f'1 #bitcoin = {bitcoin} {curr} {emoji}'
+        else:
+            text = f'{emoji} ${curr} to #satoshi conversions:' + '\n' + \
+                '\n' + f'   .01 {curr} = {hundredths_unit} sats' + \
+                '\n' + f'   1 {curr} = {single_unit} sats' + \
+                '\n' + f'   100 {curr} = {hundred_units} sats' + \
+                '\n' + f'   1,000 {curr} = {thousand_units} sats' + \
+                '\n' + '\n' + \
+                '\n' + f'   1 sat = {sats} {curr}' + \
+                '\n' + f'   100 sats = {hundred} {curr}' + \
+                '\n' + f'   1,000 sats = {thousand} {curr}' + \
+                '\n' + f'   1,000,000 sats = {million} {curr}' + \
+                '\n' + '\n' + \
+                f'1 #bitcoin = {bitcoin} {curr} {emoji}'
+
+        print(text)
+        print('length:', len(text))
+
+        # self.tweet_it(text, status.id)
 
     def tweet_it(self, text, reply_id):
         # reply_id = str(reply_id)
@@ -121,7 +179,4 @@ myStreamListener = MyStreamListener()
 myStream = tp.Stream(auth=api.auth, listener=myStreamListener)
 
 
-myStream.filter(track=['@1satoshibot'], is_async=True)
-
-# my user id
-# myStream.filter(follow=["1035658046515466240"])
+myStream.filter(track=['USD'], is_async=True)

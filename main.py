36aa061b69d2606b4285e7c0343572b03ae4cf08
Kory Newton <korybnewton@gmail.com>
2019-08-27 16:24:06 -0700

    remove test stream

--- main.py
+++ main.py
@@ -209,7 +209,7 @@ if __name__ == "__main__":
     stream = tp.Stream(auth, listener)
 
     print('streaming')
-    stream.filter(track=['cat'], is_async=True)
+    stream.filter(track=['@1satoshibot'], is_async=True)
 
     while True:
         num = random.randint(0, 10)

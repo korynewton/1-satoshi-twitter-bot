3cb6071d64a9b03c8b52c5c725dd949155506844
Kory Newton <korybnewton@gmail.com>
2020-02-07 15:23:04 -0800

    add more logging to daily script

--- daily.py
+++ daily.py
@@ -8,15 +8,21 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tp.API(auth)
 
 # sleep
+print("daily script started")
 hours = random.randint(1,4) * 60 * 60
 minutes = random.randint(1,60)*60
+total_sleep = hours + minutes
+print(f"sleeping for {total_sleep} seconds...")
 time.sleep(hours+minutes)
 
 #select currency from list and tweet
 daily_show = ['USD', 'CAD', 'JPY', 'CNY', 'MXN', 'RUB', 'EUR', 'AUD', 'INR']
 currency = random.choice(daily_show)
+tweet = f"@1satoshibot show {currency}"
+
 try:
-    api.update_status(f"@1satoshibot show {currency}")
+    print(f"tweeting: {tweet}")
+    api.update_status(tweet)
 except:
     print(f'error tweeting daily (Currency={currency}')
 
